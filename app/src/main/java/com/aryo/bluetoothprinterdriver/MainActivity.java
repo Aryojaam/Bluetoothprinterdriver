2ec1793fd549d8b45eae8530140259939f8d9996
Aryo <aryojaam@gmail.com>
2020-02-04 17:49:55 +0100

    activate bluetooth and check for already paired devices

--- app/src/main/java/com/aryo/bluetoothprinterdriver/MainActivity.java
+++ app/src/main/java/com/aryo/bluetoothprinterdriver/MainActivity.java
@@ -2,13 +2,40 @@ package com.aryo.bluetoothprinterdriver;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
+    BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+    Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
+    public int REQUEST_ENABLE_BT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void pair_bluetooth(View view) {
+        if (bluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+            Log.d("Bluetooth", "device doesnt support bt");
+        } else {
+            if (!bluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+            for (BluetoothDevice device : pairedDevices) {
+                String deviceName = device.getName();
+                String deviceHardwareAddress = device.getAddress(); // MAC Address
+                Log.d("Bluetooth", "sendMessage: Device: " + deviceName + " " + deviceHardwareAddress);
+            }
+        }
     }
 }
